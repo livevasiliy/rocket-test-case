99bcb9089157e28cc7c3286612e4c848d0cf65e5
Vasiliy Pivovarov <livevasiliy@yandex.ru>
2020-07-28 22:20:52 +0400

    Updates

---
+++ precache-manifest.a12c3b97f9362bf0dc933b6e8509bcc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f1791f24ab7dd8a6996abd2ee416e5",
+    "url": "/rocket-test-case/index.html"
+  },
+  {
+    "revision": "3fe8098aa5b2626ca707",
+    "url": "/rocket-test-case/static/css/main.2b9d050d.chunk.css"
+  },
+  {
+    "revision": "246fdbeab9a8d9889c1f",
+    "url": "/rocket-test-case/static/js/2.a833b3e0.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/rocket-test-case/static/js/2.a833b3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe8098aa5b2626ca707",
+    "url": "/rocket-test-case/static/js/main.2f34b7f9.chunk.js"
+  },
+  {
+    "revision": "a172201fdb0e4b650de8",
+    "url": "/rocket-test-case/static/js/runtime-main.d8a70945.js"
+  },
+  {
+    "revision": "104e26e53be7da53ff63697bf10c11ae",
+    "url": "/rocket-test-case/static/media/github.104e26e5.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rocket-test-case/static/media/logo.5d5d9eef.svg"
+  }
+]);
