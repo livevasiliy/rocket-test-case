23e1f4f711f58222dd7ee33c44e2239a0dcbd3d9
Vasiliy Pivovarov <livevasiliy@yandex.ru>
2020-07-28 21:41:57 +0400

    Updates

---
+++ precache-manifest.e5f84db128a1a1f53461a61b933b3a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34acac405f8c08a89a29a8ef9c46b5b",
+    "url": "/rocket-test-case/index.html"
+  },
+  {
+    "revision": "841ec2fbad6a03869c04",
+    "url": "/rocket-test-case/static/css/main.2b9d050d.chunk.css"
+  },
+  {
+    "revision": "246fdbeab9a8d9889c1f",
+    "url": "/rocket-test-case/static/js/2.a833b3e0.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/rocket-test-case/static/js/2.a833b3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841ec2fbad6a03869c04",
+    "url": "/rocket-test-case/static/js/main.229026b0.chunk.js"
+  },
+  {
+    "revision": "a172201fdb0e4b650de8",
+    "url": "/rocket-test-case/static/js/runtime-main.d8a70945.js"
+  },
+  {
+    "revision": "104e26e53be7da53ff63697bf10c11ae",
+    "url": "/rocket-test-case/static/media/github.104e26e5.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/rocket-test-case/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- src/models/IProject.ts
+++
@@ -1,6 +0,0 @@
-export interface IProject {
-    userId: number,
-    id: number,
-    title: string,
-    body: string
-}
